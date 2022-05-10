943c0298bbfa03207a797435a98deac10f30303a
AnvarZZ <anvarzeynalzade@gmail.com>
2022-05-10 10:27:38 +0400

    Add click listeners

--- app/src/main/java/az/brains/gitdemo3/MainActivity.kt
+++ app/src/main/java/az/brains/gitdemo3/MainActivity.kt
@@ -1,11 +1,16 @@
 package az.brains.gitdemo3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv.setOnClickListener {
+            Intent(this, MainActivity2::class.java).apply { startActivity(this) }
+        }
     }
 }
